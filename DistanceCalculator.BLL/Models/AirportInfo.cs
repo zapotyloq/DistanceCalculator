6f539f9666ce07b194aefb5b91660ae5138d5162
Александр Запотылок <al3xzap@gmail.com>
2023-12-24 22:30:21 +0300

    Removed unnecessary usings.

--- DistanceCalculator.BLL/Models/AirportInfo.cs
+++ DistanceCalculator.BLL/Models/AirportInfo.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace DistanceCalculator.BLL.Models
+﻿namespace DistanceCalculator.BLL.Models
 {
     public class AirportInfo
     {
